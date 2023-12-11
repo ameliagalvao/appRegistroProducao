6bb07b549d20df3d1b698dddf1ea8530e7e3e8b0
amelia <ameliamgalvao@gmail.com>
2023-12-11 09:52:49 -0300

    pom dependencies

--- src/main/java/br/edu/infnet/appRegistroProducao/model/domain/Cliente.java
+++ src/main/java/br/edu/infnet/appRegistroProducao/model/domain/Cliente.java
@@ -3,9 +3,11 @@ package br.edu.infnet.appRegistroProducao.model.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import java.util.List;
 @Setter
 @Getter
+@Entity
 public class Cliente implements ClienteOuProspecto {
     private String nome;
     private String cnpjOuCpf;
