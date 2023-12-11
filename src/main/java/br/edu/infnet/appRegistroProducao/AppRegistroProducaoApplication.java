cae805431a3b1173ba19a9174e5282ad56f6db8a
amelia <ameliamgalvao@gmail.com>
2023-12-11 12:02:10 -0300

    table not being created

--- src/main/java/br/edu/infnet/appRegistroProducao/AppRegistroProducaoApplication.java
+++ src/main/java/br/edu/infnet/appRegistroProducao/AppRegistroProducaoApplication.java
@@ -2,8 +2,10 @@ package br.edu.infnet.appRegistroProducao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br.edu.infnet.appRegistroProducao.model.domain")
 public class AppRegistroProducaoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/edu/infnet/appRegistroProducao/model/domain/Cliente.java
+++ src/main/java/br/edu/infnet/appRegistroProducao/model/domain/Cliente.java
@@ -4,11 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 @Setter
 @Getter
 @Entity
 public class Cliente implements ClienteOuProspecto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCliente;
     private String nome;
     private String cnpjOuCpf;
     private String telefone;
